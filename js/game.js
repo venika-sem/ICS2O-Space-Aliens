fb962568f09dc6d2e9ae52cb7f4f1dcf73f6e980
Venika Sem <99354917+venika-sem@users.noreply.github.com>
2022-04-20 23:55:16 +0000

    changed js

--- js/game.js
+++ js/game.js
@@ -6,15 +6,25 @@
 // Created on: Apr 2022
 // This is the Phaser3 configuration file 
 
-/* global Phaser */
-
 //* Game scene */
 const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
-  backgroundColor: 0x5f6e7a
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: true
+    }
+},
+  // set background color
+  backgroundColor: 0x5f6e7a,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    // we place in the middle of the page.
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  }
 }
-
+  
 const game = new Phaser.Game(config)
 console.log(game)
