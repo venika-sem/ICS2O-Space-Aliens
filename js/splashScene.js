1791a873267b04448c48cc20db8aed9e6b725374
Venika Sem <99354917+venika-sem@users.noreply.github.com>
2022-04-22 17:14:34 +0000

    changed js

---
+++ js/splashScene.js
@@ -0,0 +1,29 @@
+/* global Phaser */
+
+// Copyright (c) 2022 Venika Sem All rights reserved
+//
+// Created by: Venika Sem
+// Created on: Apr 2022
+// This is the Splash Scene 
+
+class Splashscene extends Phaser.scene {
+  constructor () {
+    super({ key: 'splashScene' })
+  }
+
+  init (data) {
+    this.cameras.main.setBackgroundColor('#ffffff')
+  }
+
+  preload () {
+    console.log('Splash Scene')
+  }
+
+  create (data) {
+  }
+
+  upate (time, delta) {
+  }
+}
+
+export default SplashScene
