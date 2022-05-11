439d545216ef1ba39db3f99d77d4352a95f9e88e
Venika Sem <99354917+venika-sem@users.noreply.github.com>
2022-05-11 00:55:09 +0000

    changed js

--- js/gameScene.js
+++ js/gameScene.js
@@ -35,7 +35,23 @@ class GameScene extends Phaser.Scene {
   }
 
   update (time, delta) {
-  }
+    // called 60 times a second, hopefully!
+
+    const keyLeftObj = this.input.keyboard.addKey('LEFT')
+    const keyRightObj = this.input.keyboard.addKey('RIGHT')
+
+    if (keyLeftObj.isDown === true) {
+      this.ship.x -= 15
+      if (this.ship.x < 0) {
+        this.ship.x = 0
+        }
+    }
+    if (keyRightObj.isDown === true) {
+      this.ship.x += 15
+      if (this.ship.x > 0) {
+        this.ship.x = 1920
+        }
+    }
 }
 
 export default GameScene
