dc22615bb33c24d511b6a3096855ab030e9d9b92
Venika Sem <99354917+venika-sem@users.noreply.github.com>
2022-05-27 16:40:58 +0000

    changed js

--- js/gameScene.js
+++ js/gameScene.js
@@ -7,17 +7,6 @@
 // This is the Game Scene 
 
 class GameScene extends Phaser.Scene {
-  // create an alien
-  createAlien () {
-    const alienXLocation = Math.floor(Math.random() * 1920) + 1 // this will get a number between 1 and 1920;
-    let alienXVelocity = Math.floor(Math.random() * 50) + 1 // this will get a number between 1 and 50;
-    alienXVelocity *= Math.round(Math.random()) ? 1 : -1 // this will add minus sign in 50% of cases
-    const anAlien = this.physics.add.sprite(alienXLocation, -100, 'alien')
-    anAlien.body.velocity.y = 200
-    anAlien.body.velocity.x = alienXVelocity
-    this.alienGroup.add(anAlien)
-  }
-  
   constructor () {
     super({ key: 'gameScene' })
 
@@ -37,9 +26,6 @@ class GameScene extends Phaser.Scene {
     this.load.image('starBackground', 'assets/starBackground.png')
     this.load.image('ship', 'assets/spaceShip.png')
     this.load.image('missile', 'assets/missile.png')
-    this.load.image('alien', 'assets/alien.png')
-    // sound
-    this.load.audio('laser', 'assets/laser1.wav')
   }
 
   create (data) {
@@ -52,13 +38,9 @@ class GameScene extends Phaser.Scene {
     this.missileGroup = this.physics.add.group()
   }
 
-  // create a group for the aliens
-  this.alienGroup = this.add.group()
-  this.createAlien()
-}
-
   update (time, delta) {
     // called 60 times a second, hopefully!
+    
     const keyLeftObj = this.input.keyboard.addKey('LEFT')
     const keyRightObj = this.input.keyboard.addKey('RIGHT')
     const keySpaceObj = this.input.keyboard.addKey('SPACE')
@@ -82,20 +64,12 @@ class GameScene extends Phaser.Scene {
         this.fireMissile = true
         const aNewMissile = this.physics.add.sprite(this.ship.x, this.ship.y, 'missile')
         this.missileGroup.add(aNewMissile)
-        this.sound.play('laser')
       }
     }
   
     if (keySpaceObj.isUp === true) {
       this.fireMissile = false
     }
-
-    this.missileGroup.children.each(function (item) {
-      item.y = item.y - 15
-      if (item.y < 0) {
-        item.destroy()
-      }
-    })
   }
 }
 
