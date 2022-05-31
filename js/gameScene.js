dbb53c6866c6ef7a235cebf0f404e3649ce15bf9
Venika Sem <99354917+venika-sem@users.noreply.github.com>
2022-05-31 01:18:20 +0000

    changed js

--- js/gameScene.js
+++ js/gameScene.js
@@ -21,13 +21,15 @@ class GameScene extends Phaser.Scene {
   constructor () {
     super({ key: 'gameScene' })
 
-    this.background = null
     this.ship = null
     this.fireMissile = false
+    this.score = 0
+    this.scoreText = 0
+    this.scoreTextStyle = { font: '65px Arial', fill: '#ffffff', align: 'center' }
   }
 
   init (data) {
-    this.cameras.main.setBackgroundColor('#ffffff')
+    this.cameras.main.setBackgroundColor('#0x5f6e7a')
   }
 
   preload () {
@@ -40,24 +42,37 @@ class GameScene extends Phaser.Scene {
     this.load.image('alien', 'assets/alien.png')
     // sound
     this.load.audio('laser', 'assets/laser1.wav')
+    this.load.audio('explosion', 'assets/barrelExploding.wav')
   }
 
   create (data) {
     this.background = this.add.image(0, 0, 'starBackground').setScale(2.0)
     this.background.setOrigin(0, 0)
 
+    this.scoreText = this.add.text(10, 10, 'Score: ' + this.score.toString(), this.scoreTextStyle)
+
     this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, 'ship')
 
     // create a group for the missiles
     this.missileGroup = this.physics.add.group()
-  }
 
-    //create a group for the missiles
+    // create a group for the aliens
     this.alienGroup = this.add.group()
     this.createAlien()
-  }
 
-  update (time, delta) {
+    // Collisions between missiles and aliens
+    this.physics.add.collider(this.missileGroup, this.alienGroup, function (missileCollide, alienCollide) {
+      alienCollide.destroy()
+      missileCollide.destroy()
+      this.sound.play('explosion')
+      this.score = this.score + 1
+      this.scoreText.setText('Score: ' + this.score.toString())
+      this.createAlien()
+      this.createAlien()
+    }.bind(this))
+  }
+  
+  update (time, delta) {          
     // called 60 times a second, hopefully!
     
     const keyLeftObj = this.input.keyboard.addKey('LEFT')
